87b31a8af54a8b9339429f8159faa6d382c23587
Steve Carratt <scarratt@simplifydigital.co.uk>
2015-01-26 17:44:00 +0000

    created tests

---
+++ test/components/provider-carousel-test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by scarratt on 26/01/2015.
+ */
+
+(function () {
+    define(['knockout', './../../src/components/provider-carousel/provider-carousel.js'], function (ko, component) {
+
+        describe('provider carousel component', function() {
+
+            it('expect component to be defined', function() {
+                expect(component).toBeDefined();
+            })
+
+        })
+
+    })
+}());

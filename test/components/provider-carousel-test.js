bf2a70158353fc6e36cd9f053666661793fa2a3f
Steve Carratt <scarratt@simplifydigital.co.uk>
2015-01-26 17:51:44 +0000

    added first test

--- test/components/provider-carousel-test.js
+++ test/components/provider-carousel-test.js
@@ -3,15 +3,42 @@
  */
 
 (function () {
-    define(['knockout', './../../src/components/provider-carousel/provider-carousel.js'], function (ko, component) {
+    define(['knockout', './../../src/components/provider-carousel/provider-carousel.js', 'jquery'], function (ko, component, $) {
+        var ProviderCarouselViewModel = component.viewModel;
+        var componentName = 'provider-carousel';
+        component.synchronous = true;
+        ko.components.register(componentName, component);
 
-        describe('provider carousel component', function() {
+        describe('provider carousel component', function () {
+            var fixture;
 
-            it('expect component to be defined', function() {
-                expect(component).toBeDefined();
-            })
+            beforeEach(function () {
+                fixture = setFixtures('<' + componentName + ' params="{providers: providers}"></' + componentName + '>');
 
-        })
 
+            });
+
+            describe('when component loads', function() {
+
+                beforeEach(function() {
+                    ko.applyBindings({providers: expectedProviders}, $('#jasmine-fixtures')[0]);
+                });
+
+                it('has a img element for each provider provided', function() {
+                    expect($('img').length).toBe(expectedProviders.length);
+                })
+            });
+        });
+
+        var expectedProviders = [
+            {
+                name: 'provider1',
+                logoUrl: 'http://testlogo.com'
+            },
+            {
+                name: 'provider2',
+                logoUrl: 'http://testlogo.com'
+            }
+        ];
     })
 }());

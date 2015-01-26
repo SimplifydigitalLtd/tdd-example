699c368ec2818637130b9ea86f060222396f6f38
Steve Carratt <scarratt@simplifydigital.co.uk>
2015-01-26 17:58:26 +0000

    added test to check that provider has the correct src

--- test/components/provider-carousel-test.js
+++ test/components/provider-carousel-test.js
@@ -26,18 +26,23 @@
 
                 it('has a img element for each provider provided', function() {
                     expect($('img').length).toBe(expectedProviders.length);
-                })
+                });
+
+                it('has the src of each img element set to the value for that provider', function() {
+                    expect($("img[src='"+ expectedProviders[0].logoUrl +"']").length).toBe(1);
+                    expect($("img[src='"+ expectedProviders[1].logoUrl +"']").length).toBe(1);
+                });
             });
         });
 
         var expectedProviders = [
             {
                 name: 'provider1',
-                logoUrl: 'http://testlogo.com'
+                logoUrl: 'http://testlogo1.com'
             },
             {
                 name: 'provider2',
-                logoUrl: 'http://testlogo.com'
+                logoUrl: 'http://testlogo2.com'
             }
         ];
     })
